00f86087c6afa20a2d6bbfc90748be344522870f
James Zhan <James.Zhan@adrenalinmedia.com.au>
2018-05-17 14:48:31 +1000

    init deep link

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 // @flow
 
 import React, { Component } from 'react';
+import { Linking } from 'react-native';
 import { Provider } from 'react-redux';
 import navigatorUtils from './utils/NavigationUtils';
 import store from './configureStore';
@@ -8,6 +9,20 @@ import TopNavigator from './TopNavigator';
 
 
 export default class App extends Component<*> {
+  componentDidMount() {
+    Linking.getInitialURL().then((url) => {
+      if (url) {
+        this.handleOpenURL({ url });
+      }
+    }).catch(err => console.error('An error occurred', err));
+    Linking.addEventListener('url', this.handleOpenURL);
+  }
+  componentWillUnmount() {
+    Linking.removeEventListener('url', this.handleOpenURL);
+  }
+  handleOpenURL(event) {
+    console.log(event.url);
+  }
   render() {
     return (
       <Provider store={store}>
